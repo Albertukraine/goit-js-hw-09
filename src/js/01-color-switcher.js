f2d24cf7d4f24089ea2235884e19045ef4e88ebc
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2022-09-11 18:05:06 +0300

    resolve task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+  bodyEl: document.querySelector('body'),
+  startBtnEl: document.querySelector('[data-start]'),
+  stopBtnEl: document.querySelector('[data-stop]'),
+};
 
+const DURATION = 1000;
+let currentColor = null;
+let timerID = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function addClassOnBody() {
+  currentColor = getRandomHexColor();
+  refs.bodyEl.style.backgroundColor = currentColor;
+}
+
+refs.startBtnEl.addEventListener('click', () => {
+  timerID = setInterval(() => {
+    addClassOnBody();
+  }, DURATION);
+  refs.startBtnEl.disabled = true;
+});
+
+refs.stopBtnEl.addEventListener('click', () => {
+  clearInterval(timerID);
+  refs.startBtnEl.disabled = false;
+});
+// console.log(refs.bodyEl);
+// console.log(refs.startBtnEl);
+// console.log(refs.stopBtnEl);
